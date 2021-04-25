2bd778b4e707d3cb58badc9393694740b526c69c
abdalrahmansamara <abdsamara_1998@yahoo.com>
2021-04-25 15:29:29 +0300

    Local JSON

---
+++ server.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const cors = require('cors');
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`you are listening to PORT ${PORT}`)
+})
+
+// Routes
+
+app.get('/location', locationHandler)
+app.get('/weather', weatherHandler)
+app.get('*', errorHandler)
+
+// Handler Functions
+
+function locationHandler (req,res) {
+  const locationData = require('./data/location.json');
+
+  let newLocation = new Location(locationData)
+
+  res.json(newLocation);
+}
+
+function weatherHandler (req,res) {
+  let weatherData = require('./data/weather.json')
+  let results = [];
+  weatherData.data.forEach(element => {
+    let newData = new Weather(element);
+    results.push(newData);
+  })
+  res.send(results);
+}
+
+function errorHandler (req,res) {
+  let errorMessage = {
+    status: 500,
+    responseText: 'Sorry, something went wrong'
+  };
+  res.status(500).send(errorMessage);
+}
+
+// Constructors
+
+function Location (loc){
+  this.search_query = 'Lynnwood';
+  this.formatted_query = loc[0].display_name;
+  this.latitude = loc[0].lat;
+  this.longitude = loc[0].lon;
+}
+function Weather (weatherDat) {
+  this.forecast = weatherDat.weather.description;
+  this.time = weatherDat.datetime;
+}
